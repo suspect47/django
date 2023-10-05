58583d26458633e15820ffbd7fd1ec3fcb58e466
igigonin <igigonin@solution.sbt>
2023-10-05 15:08:18 +0300

    working with templates last

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -3,9 +3,6 @@ from MainApp import views
 
 urlpatterns = [
     path("", views.home),
-    #path("about", views.about),
-    #path("item/<int:number>", views.item),
-    #path("items", views.numeric_list),
     path("item/<int:number>", views.item1),
     path("items", views.numeric_list),
     path("index.html", views.index)
